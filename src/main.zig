75e5510584d8ef6cca83f505f4516c9f440b0234
Jack DeVries <jdevries3133@gmail.com>
2025-10-22 07:15:56 -0400

    feat: switch to arena allocator
    The official docs recommend this for short-lived CLI programs like this.

--- src/main.zig
+++ src/main.zig
@@ -39,18 +39,17 @@ test "parse zon config file" {
 }
 
 pub fn main() !void {
-    var buffer: [32_768]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    const allocator = fba.allocator();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
     const fp = try std.fs.openFileAbsolute("/Users/johndevries/repos/quickzilver/config.zon", .{});
     const stat = try fp.stat();
     var config_bytes = try allocator.alloc(u8, stat.size + 1);
-    defer allocator.free(config_bytes);
     _ = try fp.read(config_bytes);
     config_bytes[stat.size] = 0;
     const config_str = config_bytes[0..stat.size :0];
     const conf = try parse(allocator, config_str);
-    std.debug.print("{d}.{d}.{d}\n", .{conf.version_major, conf.version_minor, conf.version_patch});
+    std.debug.print("{d}.{d}.{d}\n", .{ conf.version_major, conf.version_minor, conf.version_patch });
 }
 
