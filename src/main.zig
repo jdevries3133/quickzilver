6254c1d674988872623a1955aa16fc33a3709530
Jack DeVries <jdevries3133@gmail.com>
2025-11-01 09:38:01 -0400

    chore: move debugging to the bottom

--- src/main.zig
+++ src/main.zig
@@ -57,47 +57,6 @@ test main {
     try main();
 }
 
-////////////////////////////////// debugging //////////////////////////////////
-
-const debugPrinting: enum { Enabled, Disabled } = blk: {
-    if (builtin.is_test) {
-        break :blk .Enabled;
-    }
-    break :blk .Disabled;
-};
-
-/// Get `loc` by calling the `@src()` builtin.
-fn dbg(comptime loc: std.builtin.SourceLocation, comptime fmt: []const u8, args: anytype) void {
-    comptime {
-        std.debug.assert(fmt[fmt.len - 1] == '\n'); // fmt template must end with newline
-    }
-    if (debugPrinting != .Enabled) {
-        return;
-    }
-
-    const prefixed_fmt = comptime pf: {
-        const f = loc.file;
-
-        const col = loc.column;
-        var col_strbuf: [10]u8 = undefined;
-        const col_str = std.fmt.bufPrint(&col_strbuf, "{d}", .{col}) catch unreachable;
-        const ln = loc.line;
-        var ln_strbf: [10]u8 = undefined;
-        const ln_str = std.fmt.bufPrint(&ln_strbf, "{d}", .{ln}) catch unreachable;
-
-        const mod = loc.module;
-        const func = loc.fn_name;
-
-        var fmt_buf: [
-            f.len + col_str.len + ln_str.len + mod.len + func.len + 14 + fmt.len
-        ]u8 = undefined;
-        _ = std.fmt.bufPrint(&fmt_buf, "src/{s}:{s}:{s} || {s}::{s}\t{s}\n", .{ f, ln_str, col_str, mod, func, fmt }) catch unreachable;
-        const final = fmt_buf;
-        break :pf final;
-    };
-    std.debug.print(&prefixed_fmt, args);
-}
-
 ////////////////////////////////// config /////////////////////////////////////
 
 const Config = struct {
@@ -324,3 +283,45 @@ fn skip_if_offline(comptime loc: std.builtin.SourceLocation, alloc: std.mem.Allo
     };
     defer list.deinit();
 }
+
+////////////////////////////////// debugging //////////////////////////////////
+
+const debugPrinting: enum { Enabled, Disabled } = blk: {
+    if (builtin.is_test) {
+        break :blk .Enabled;
+    }
+    break :blk .Disabled;
+};
+
+/// Get `loc` by calling the `@src()` builtin.
+fn dbg(comptime loc: std.builtin.SourceLocation, comptime fmt: []const u8, args: anytype) void {
+    comptime {
+        std.debug.assert(fmt[fmt.len - 1] == '\n'); // fmt template must end with newline
+    }
+    if (debugPrinting != .Enabled) {
+        return;
+    }
+
+    const prefixed_fmt = comptime pf: {
+        const f = loc.file;
+
+        const col = loc.column;
+        var col_strbuf: [10]u8 = undefined;
+        const col_str = std.fmt.bufPrint(&col_strbuf, "{d}", .{col}) catch unreachable;
+        const ln = loc.line;
+        var ln_strbf: [10]u8 = undefined;
+        const ln_str = std.fmt.bufPrint(&ln_strbf, "{d}", .{ln}) catch unreachable;
+
+        const mod = loc.module;
+        const func = loc.fn_name;
+
+        var fmt_buf: [
+            f.len + col_str.len + ln_str.len + mod.len + func.len + 14 + fmt.len
+        ]u8 = undefined;
+        _ = std.fmt.bufPrint(&fmt_buf, "src/{s}:{s}:{s} || {s}::{s}\t{s}\n", .{ f, ln_str, col_str, mod, func, fmt }) catch unreachable;
+        const final = fmt_buf;
+        break :pf final;
+    };
+    std.debug.print(&prefixed_fmt, args);
+}
+
